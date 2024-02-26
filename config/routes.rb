fed632238e19230d30e42ad387c933340fc27c31
Dado Biba <bibad.pro@gmail.com>
2024-02-26 15:40:51 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources
   resources :cars, only: %i[index show new create] do
     resources :bookings, only: %i[new create]
   end
@@ -14,5 +12,5 @@ Rails.application.routes.draw do
   resources :bookings, only: %i[index]
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "cars#index"
 end
