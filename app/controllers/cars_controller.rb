dc910062640da6e7b017b5eb449adf2e3b5c2707
Dado Biba <bibad.pro@gmail.com>
2024-02-27 15:48:44 +0100

    user picture

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -16,9 +16,6 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params.with_defaults(user_id: User.first.id))
-
-
-
     if @car.save
       redirect_to car_path(@car)
     else
