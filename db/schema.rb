95cb4881beef3a70b62621883f2bc14fdcde429e
Samuel Braniste <samuel.braniste@gmail.com>
2024-02-27 15:24:49 +0100

    filter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_100804) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_135334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_100804) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.string "address"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,70 @@
 # Assurez-vous que Faker est requis si vous n'avez pas déjà
 require 'faker'
 
-# Dans votre fichier de seed, pour créer des images différentes pour chaque voiture
-Car.find_each.with_index(1) do |car, index|
-  # Utilisez l'index pour générer une image unique
-  unique_image_url = Faker::LoremFlickr.image(size: "300x300", search_terms: ['car'], match_all: true) + "?random=#{index}"
-  car.update(image_url: unique_image_url)
+
+# Effacer les données existantes
+User.delete_all
+Car.delete_all
+Booking.delete_all
+
+# Création de 50 utilisateurs
+50.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password_digest: BCrypt::Password.create('password'),
+    phone_number: Faker::PhoneNumber.phone_number,
+    valid_driver_license: Faker::Boolean.boolean
+  )
 end
+
+puts "50 utilisateurs créés."
+
+# Récupération des IDs des utilisateurs
+user_ids = User.pluck(:id)
+
+# Création de 70 voitures
+70.times do
+  Car.create!(
+    model: Faker::Vehicle.model,
+    brand: Faker::Vehicle.make,
+    price: Faker::Commerce.price(range: 5000..30000),
+    user_id: user_ids.sample # Sélectionne un user_id aléatoire parmi la liste
+  )
+end
+
+puts "70 voitures créées."
+
+# Récupération des IDs de 50 voitures aléatoirement pour les réservations
+car_ids_for_booking = Car.pluck(:id).sample(50)
+
+# Création de réservations pour 50 voitures sélectionnées
+car_ids_for_booking.each do |car_id|
+  start_date = Faker::Date.backward(days: 14) # Date de début dans les 14 jours précédents
+  end_date = start_date + rand(1..10).days # Date de fin entre 1 et 10 jours après la date de début
+
+  Booking.create!(
+    car_id: car_id,
+    user_id: user_ids.sample, # Sélectionne un user_id aléatoire parmi la liste
+    start_date: start_date,
+    end_date: end_date,
+    status: ['confirmed', 'pending', 'cancelled'].sample # Statut aléatoire
+  )
+end
+
+puts "50 réservations créées."
+
+
+# Ou si vous préférez utiliser une image aléatoire pour chaque voiture (via Faker) :
+Car.find_each do |car|
+  # Génère une nouvelle URL d'image aléatoire pour chaque voiture
+  random_image_url = Faker::LoremFlickr.image(size: "300x300", search_terms: ['car'])
+
+  # Met à jour la voiture avec l'URL d'image
+  car.update(image_url: random_image_url)
+
+  # Si vous préférez utiliser la même URL fixe pour toutes les voitures, remplacez `random_image_url` par `fixed_image_url` dans la ligne ci-dessus.
+end
+
 puts "Toutes les voitures ont été mises à jour avec des URLs d'image."
