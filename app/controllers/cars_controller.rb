f16ea1a08f5fe528fdd7cdd676ed9cd50e47d05f
Dado Biba <bibad.pro@gmail.com>
2024-02-28 12:32:45 +0100

    .

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -32,7 +32,7 @@ class CarsController < ApplicationController
     if @car.save
       redirect_to bookings_index_path(@car)
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessablesr_entity
     end
   end
 
