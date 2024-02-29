c6037df4db9912f8abbb98f56074f8920a4496d4
Dado Biba <bibad.pro@gmail.com>
2024-02-29 16:57:06 +0100

    changement class front

--- app/javascript/controllers/switch_controller.js
+++ app/javascript/controllers/switch_controller.js
@@ -18,19 +18,22 @@ export default class extends Controller {
   showCards() {
     this.mapTarget.classList.add("d-none")
     this.cardsTarget.classList.remove("d-none")
-    this.tabsTarget.children[0].classList.add("btn-primary")
-    this.tabsTarget.children[0].classList.remove("btn-outline-primary")
-    this.tabsTarget.children[1].classList.add("btn-outline-primary")
-    this.tabsTarget.children[1].classList.remove("btn-primary")
+    console.log(this.tabsTarget.children[1]);
+    this.tabsTarget.children[0].classList.add("active")
+    // this.tabsTarget.children[0].classList.remove("btn-outline-primary")
+    // this.tabsTarget.children[1].classList.add("btn-outline-primary")
+    this.tabsTarget.children[1].classList.remove("active")
   }
 
 
   showMap() {
     this.mapTarget.classList.remove("d-none")
     this.cardsTarget.classList.add("d-none")
-    this.tabsTarget.children[0].classList.remove("btn-primary")
-    this.tabsTarget.children[0].classList.add("btn-outline-primary")
-    this.tabsTarget.children[1].classList.remove("btn-outline-primary")
-    this.tabsTarget.children[1].classList.add("btn-primary")
+    this.tabsTarget.children[1].classList.add("active")
+    this.tabsTarget.children[0].classList.remove("active")
+    // this.tabsTarget.children[0].classList.remove("btn-primary")
+    // this.tabsTarget.children[0].classList.add("btn-outline-primary")
+    // this.tabsTarget.children[1].classList.remove("btn-outline-primary")
+    // this.tabsTarget.children[1].classList.add("btn-primary")
   }
 }
