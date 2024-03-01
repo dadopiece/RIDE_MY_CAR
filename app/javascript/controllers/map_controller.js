0c8da97e0ee2773306bd4768ce465b9193cef844
Samuel Braniste <samuel.braniste@gmail.com>
2024-03-01 15:10:34 +0100

    seedcar

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -20,12 +20,25 @@ export default class extends Controller {
   }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
-      new mapboxgl.Marker()
+      // Créez un élément div pour chaque marqueur et ajoutez le prix
+      const el = document.createElement('div');
+      el.className = 'marker';
+      el.style.width = '50px'; // La largeur du marqueur
+      el.style.height = '50px'; // La hauteur du marqueur
+      el.style.backgroundColor = "#FFF"; // La couleur de fond
+      el.style.color = "black"; // La couleur du texte
+      el.style.display = "flex";
+      el.style.justifyContent = "center";
+      el.style.alignItems = "center";
+      el.style.borderRadius = "50%"; // Pour un marqueur rond
+      el.style.border = "3px solid black"; // Bordure du marqueur
+      el.innerText = `${marker.price} €`; // Ajoutez le prix ici
+
+      // Utilisez l'élément personnalisé pour le marqueur
+      new mapboxgl.Marker(el)
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // Add this
-        .addTo(this.map)
-    })
+        .addTo(this.map);
+    });
   }
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
